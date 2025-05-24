6cc232330ae6458f0557c1d31e24cfed7666e2f7
aschwarm <aschwarm@gmail.com>
2025-05-24 12:42:08 -0700

    Finalize turtle controller and spawner + tuning of P controller

--- turtlesim_catch_them_all/src/turtle_controller.cpp
+++ turtlesim_catch_them_all/src/turtle_controller.cpp
@@ -2,21 +2,22 @@
 #include "turtlesim/msg/pose.hpp"
 #include <geometry_msgs/msg/twist.hpp>
 #include <cmath>
+#include "turtle_interfaces/msg/turtle_array.hpp"
+#include "turtle_interfaces/msg/turtle.hpp"
+#include "turtle_interfaces/srv/catch_turtle.hpp"
 
 #define PI 3.1415926535f
 
 using namespace std::placeholders;
+using namespace std::chrono_literals;
 
 class TurtleControllerNode : public rclcpp::Node
 {
     public:
-        TurtleControllerNode() : Node("turtle_controller")
+        TurtleControllerNode() : Node("turtle_controller"), current_pose_(nullptr)
         {
-            this->target_x_ = 8.5;
-            this->target_y_ = 7.5;
-            this->current_pose_ = nullptr;
-            this-> lin_Kp_ = 1.0;
-            this-> ang_Kp_ = 1.0;
+            this-> lin_Kp_ = 2.0;
+            this-> ang_Kp_ = 8.0;
             
             pose_sub_ = this->create_subscription<turtlesim::msg::Pose>("/turtle1/pose",10,
             std::bind(&TurtleControllerNode::callbackTurtle1Pose,this,_1));
@@ -25,6 +26,11 @@ class TurtleControllerNode : public rclcpp::Node
 
             control_timer_ = this->create_wall_timer(std::chrono::duration<double>(0.1),
                                 std::bind(&TurtleControllerNode::control_loop,this));
+
+            turtles_to_catch_subscriber_ = this->create_subscription<turtle_interfaces::msg::TurtleArray>("alive_turtles",10,
+            std::bind(&TurtleControllerNode::callbackTurtlesToCatch,this, _1));
+
+            catch_turtle_client_ = this->create_client<turtle_interfaces::srv::CatchTurtle>("catch_turtle", 10);
 
             RCLCPP_INFO(this->get_logger(),"Turtle contorller has been started");
         }
@@ -39,11 +45,19 @@ class TurtleControllerNode : public rclcpp::Node
 
     rclcpp::TimerBase::SharedPtr control_timer_;
 
+    rclcpp::Subscription<turtle_interfaces::msg::TurtleArray>::SharedPtr turtles_to_catch_subscriber_; 
+
+    rclcpp::Client<turtle_interfaces::srv::CatchTurtle>::SharedPtr catch_turtle_client_;
+
+    std::vector<std::shared_ptr<std::thread>> catch_turtle_thread_;
+    
     float target_x_;
     float target_y_;
 
     float lin_Kp_;
     float ang_Kp_;
+
+    turtle_interfaces::msg::Turtle turtle_to_catch_;
 
     // Methods
     void callbackTurtle1Pose(const turtlesim::msg::Pose::SharedPtr pose)
@@ -53,10 +67,10 @@ class TurtleControllerNode : public rclcpp::Node
 
     void control_loop()
     {
-        if(this->current_pose_ == nullptr) return;
+        if(this->current_pose_ == nullptr || this->turtle_to_catch_.name == "") return;
 
-        float dist_x = this->target_x_ - this->current_pose_->x;
-        float dist_y = this->target_y_ - this->current_pose_->y;
+        float dist_x = this->turtle_to_catch_.x - this->current_pose_->x;
+        float dist_y = this->turtle_to_catch_.y - this->current_pose_->y;
 
         float distance = std::sqrt(dist_x*dist_x + dist_y * dist_y);
 
@@ -67,7 +81,6 @@ class TurtleControllerNode : public rclcpp::Node
             // Position command
             cmd.linear.x = this->lin_Kp_ * distance;
 
-            
             // Orientation command
             float goal_theta = std::atan2(dist_y, dist_x);
             float diff_angle = goal_theta - this->current_pose_->theta;
@@ -87,13 +100,79 @@ class TurtleControllerNode : public rclcpp::Node
         }
         else
         {
-           // Call catch turtle service
-
            cmd.linear.x = 0.0;
            cmd.angular.z = 0.0;
+
+           catch_turtle_thread_.push_back(std::make_shared<std::thread>(
+            std::bind(&TurtleControllerNode::callCatchTurtleService, this, turtle_to_catch_.name)));
+           
+            turtle_to_catch_.name = "";
+
+
         }
 
         this->cmd_vel_publisher_->publish(cmd);
+    }
+
+    float getDistance(turtle_interfaces::msg::Turtle turtle)
+    {
+        float dist_x = turtle.x - this->current_pose_->x;
+        float dist_y = turtle.y - this->current_pose_->y;
+
+        float distance = std::sqrt(dist_x*dist_x + dist_y * dist_y);
+
+        return distance;
+    }
+    
+    void callbackTurtlesToCatch(const turtle_interfaces::msg::TurtleArray::SharedPtr turtle_list)
+    {
+        if(! turtle_list->turtles.empty())
+        {
+            
+            turtle_interfaces::msg::Turtle closest_turtle = turtle_list->turtles.at(0);
+            float closest_turtle_distance = getDistance(closest_turtle);
+            
+            for (int i = 1; i < (int)turtle_list->turtles.size(); i++)
+            {
+                float distance = getDistance(turtle_list->turtles.at(i));
+                if(distance < closest_turtle_distance)
+                {
+                    closest_turtle = turtle_list->turtles.at(i);
+                    closest_turtle_distance = distance;
+                }
+            }
+
+            this->turtle_to_catch_ = closest_turtle;
+        }
+    }
+
+    void callCatchTurtleService(std::string turtle_name)
+    {
+        while(! catch_turtle_client_->wait_for_service(1s))
+        {
+            RCLCPP_WARN(this->get_logger(), "Waiting for Catch server...");
+        }
+
+        auto request = std::make_shared<turtle_interfaces::srv::CatchTurtle::Request>();
+        request->name = turtle_name;
+
+        auto future = catch_turtle_client_->async_send_request(request);
+
+         try
+        {
+            auto response = future.get();
+
+            if(!response->success)
+            {
+                RCLCPP_ERROR(this->get_logger(), "Failed to catch turtle: %s", turtle_name.c_str());
+            }
+            
+        }
+        catch(const std::exception& e)
+        {
+            RCLCPP_ERROR(this->get_logger(), "Catch Service call failed");
+        }
+
     }
 };
 
--- turtlesim_catch_them_all/src/turtle_spawner.cpp
+++ turtlesim_catch_them_all/src/turtle_spawner.cpp
@@ -1,5 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "turtlesim/srv/spawn.hpp"
+#include "turtlesim/srv/kill.hpp"
 #include "turtle_interfaces/msg/turtle.hpp"
 #include "turtle_interfaces/msg/turtle_array.hpp"
 #include "turtle_interfaces/srv/catch_turtle.hpp"
@@ -7,6 +8,7 @@
 #define PI 3.1415926535f
 
 using namespace std::chrono_literals;
+using namespace std::placeholders;
 
 class TurtleSpawnerNode : public rclcpp::Node
 {
@@ -16,10 +18,15 @@ class TurtleSpawnerNode : public rclcpp::Node
             this->turtlePrefix = "target";
             
             spawn_client_ = this->create_client<turtlesim::srv::Spawn>("/spawn");
-            spawn_timer_ = this->create_wall_timer(std::chrono::duration<double>(2.0), 
+            spawn_timer_ = this->create_wall_timer(std::chrono::duration<double>(3.0), 
                             std::bind(&TurtleSpawnerNode::spawnNewTurtle, this));
 
-            alive_turtle_publisher_ = this->create_publisher<turtle_interfaces::msg::TurtleArray>("alive_turrtles",10);
+            alive_turtle_publisher_ = this->create_publisher<turtle_interfaces::msg::TurtleArray>("alive_turtles",10);
+
+            catch_turtle_service_ = this->create_service<turtle_interfaces::srv::CatchTurtle>("catch_turtle",
+            std::bind(&TurtleSpawnerNode::callbackCatchTurtle, this, _1, _2));
+            
+            kill_client_ = this->create_client<turtlesim::srv::Kill>("/kill");
         }
 
     private:
@@ -35,6 +42,10 @@ class TurtleSpawnerNode : public rclcpp::Node
     std::vector<turtle_interfaces::msg::Turtle> alive_turtles_;
 
     std::vector<std::shared_ptr<std::thread>> spawn_turtle_threads_;
+    std::vector<std::shared_ptr<std::thread>> kill_turtle_threads_;
+
+    rclcpp::Service<turtle_interfaces::srv::CatchTurtle>::SharedPtr catch_turtle_service_;
+    rclcpp::Client<turtlesim::srv::Kill>::SharedPtr kill_client_;
 
     void spawnNewTurtle()
     {
@@ -88,7 +99,6 @@ class TurtleSpawnerNode : public rclcpp::Node
             RCLCPP_ERROR(this->get_logger(), "Spawn Service call failed");
         }
         
-
     }
 
     void publishAliveTurtles()
@@ -96,6 +106,47 @@ class TurtleSpawnerNode : public rclcpp::Node
         auto msg = turtle_interfaces::msg::TurtleArray();
         msg.turtles = alive_turtles_;
         alive_turtle_publisher_->publish(msg);
+    }
+
+    void callbackCatchTurtle(const turtle_interfaces::srv::CatchTurtle::Request::SharedPtr request,
+                             const turtle_interfaces::srv::CatchTurtle::Response::SharedPtr response)
+    {
+        kill_turtle_threads_.push_back(std::make_shared<std::thread>(std::bind(&TurtleSpawnerNode::callKillTurtleService, this, request->name)));
+        response->success = true;
+    }
+
+    void callKillTurtleService(std::string turtle_name)
+    {
+        while(! kill_client_->wait_for_service(1s))
+        {
+            RCLCPP_WARN(this->get_logger(), "Waiting for kill server...");
+        }
+
+        auto request = std::make_shared<turtlesim::srv::Kill::Request>();
+        request->name = turtle_name;
+
+        auto future = kill_client_->async_send_request(request);
+
+        try
+        {
+            auto response = future.get();
+            
+            for(int i = 0; (int)alive_turtles_.size(); i++)
+            {
+                if(alive_turtles_.at(i).name == turtle_name)
+                {
+                    alive_turtles_.erase(alive_turtles_.begin() + i);
+                    publishAliveTurtles();
+                    break;
+                }
+            }
+
+        }
+        catch(const std::exception &e)
+        {
+            RCLCPP_ERROR(this->get_logger(), "Kill service call failed.");
+        }
+
     }
 
 };
